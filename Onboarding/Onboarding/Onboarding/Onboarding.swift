991a751134ab434f108323a7501c2e61f232c0f4
Joseph <gmremcorat@gmail.com>
2020-01-24 15:03:59 -0500

    Initial Onboard view created
    Initial file and instantiation of Onboard view created

---
+++ Onboarding/Onboarding/Onboarding/Onboarding.swift
@@ -0,0 +1,21 @@
+//
+//  Onboarding.swift
+//  Onboarding
+//
+//  Created by Joseph Storer on 1/24/20.
+//  Copyright © 2020 Joseph Storer. All rights reserved.
+//
+
+import UIKit
+
+class Onboarding: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
