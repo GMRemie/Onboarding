cfd05518f18657fa15cff2f6032a1bd30911b9a4
Joseph <gmremcorat@gmail.com>
2020-01-24 14:51:48 -0500

    Xcode Init
    Default project files.

---
+++ Onboarding/Onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Joseph Storer on 1/24/20.
+//  Copyright © 2020 Joseph Storer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
